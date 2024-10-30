44bdbc10d419aaaa023627865e5577115e900312
yangtaoytt <2867868802@qq.com>
2024-10-30 17:04:21 +0800

    s

---
+++ project/src/test/java/SolutionTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.assertArrayEquals;
+
+import com.hitwh.yt.Solution;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void testSortColors() {
+        Solution solution = new Solution();
+        
+        int[] input1 = {2, 0, 2, 1, 1, 0};
+        solution.sortColors(input1);
+        assertArrayEquals(new int[]{0, 0, 1, 1, 2, 2}, input1);
+
+        int[] input2 = {2, 0, 1};
+        solution.sortColors(input2);
+        assertArrayEquals(new int[]{0, 1, 2}, input2);
+
+        int[] input3 = {0};
+        solution.sortColors(input3);
+        assertArrayEquals(new int[]{0}, input3);
+
+        int[] input4 = {1};
+        solution.sortColors(input4);
+        assertArrayEquals(new int[]{1}, input4);
+
+        int[] input5 = {2, 2, 2, 1, 1, 0, 0, 0};
+        solution.sortColors(input5);
+        assertArrayEquals(new int[]{0, 0, 0, 1, 1, 2, 2, 2}, input5);
+    }
+}
